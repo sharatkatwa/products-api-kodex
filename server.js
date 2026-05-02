62b80415dbdaa1ffe162f6a3bb4163abf73caee4
sharatkatwa <sjkatwa2000@gmail.com>
2026-05-02 17:28:57 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./src/app')
+const app = require("./src/app");
 
-app.listen(3000,() =>{
-
-    console.log('server is listening to port: 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("server is listening to port:", PORT);
+});
